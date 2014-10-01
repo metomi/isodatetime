f0f440f617b735ba911b3129f8b270a18b44ad0f
Ben Fitzpatrick <ben.fitzpatrick@metoffice.gov.uk>
2014-10-01 14:54:06 +0100

    Update __init__.py
    Update for the release.

--- isodatetime/__init__.py
+++ isodatetime/__init__.py
@@ -16,4 +16,4 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #-----------------------------------------------------------------------------
 
-__version__ = "2014.08.0"
+__version__ = "2014.09.0"
