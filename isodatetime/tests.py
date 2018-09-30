26a966014e30054dcbc211b7a8e2cfabdd4e29ba
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2018-10-01 11:15:42 +1300

    Add unit tests for timezone.get_local_time_zone_format

--- isodatetime/tests.py
+++ isodatetime/tests.py
@@ -1633,6 +1633,46 @@ class TestSuite(unittest.TestCase):
             self.assertEqual(expected_hours, hours)
             self.assertEqual(expected_minutes, minutes)
 
+    # data provider for the test test_get_local_time_zone_format
+    # the format for the parameters is [tz_seconds, extended_mode, reduced_mode, expected_format]
+    get_test_get_local_time_zone_format = [
+        # UTC
+        [0, False, False, "Z"], # UTC, returns Z, the flags are never used for UTC
+        # Positive values, some with minutes != 0
+        [28800, False, False, "+0800"],  # asia/macao, +08:00
+        [28800, True, False, "+08:00"],  # asia/macao, +08:00
+        [28800, False, True, "+08"],  # asia/macao, +08:00
+        [45900, False, False, "+1245"],  # pacific/chatham, +12:45
+        [45900, True, False, "+12:45"],  # pacific/chatham, +12:45
+        [45900, False, True, "+1245"],  # pacific/chatham, +12:45 (attention as it's different in this case)
+        # Negative values, some with minutes != 0
+        [-10800, False, False, "-0300"],  # america/buenos_aires, -03:00
+        [-10800, True, False, "-03:00"],  # america/buenos_aires, -03:00
+        [-10800, False, True, "-03"],  # america/buenos_aires, -03:00
+        [-12600, False, False, "-0330"],  # america/st_johns, -03:30
+        [-12600, True, False, "-03:30"],  # america/st_johns, -03:30
+        [-12600, False, True, "-0330"]  # america/st_johns, -03:30
+    ]
+
+    @mock.patch('isodatetime.timezone.time')
+    def test_get_local_time_zone_format(self, mock_time):
+        """Test that the UTC offset string format is correct
+
+        Parts of the time module are mocked so that we can specify scenarios
+        with certain timezone seconds offsets. Daylight saving time is not really
+        important for this test case"""
+        for tz_seconds, extended_mode, reduced_mode, expected_format in self.get_test_get_local_time_zone_format:
+            # for a pre-defined timezone
+            mock_time.timezone.__neg__.return_value = tz_seconds
+            # time without dst
+            mock_time.daylight = False
+            # and localtime also without dst
+            mock_localtime = mock.Mock()
+            mock_time.localtime.return_value = mock_localtime
+            mock_localtime.tm_isdst = 0
+            tz_format = timezone.get_local_time_zone_format(extended_mode, reduced_mode)
+            self.assertEqual(expected_format, tz_format)
+
 
 def assert_equal(data1, data2):
     """A function-level equivalent of the unittest method."""
