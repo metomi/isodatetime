b7640e7edf2f8642e091c1d16256e3e836bafb65
Oliver Sanders <oliver.sanders@metoffice.gov.uk>
2020-06-08 17:54:07 +0100

    tests: remove --runslow - use -m slow

--- conftest.py
+++ conftest.py
@@ -19,21 +19,3 @@
 """Local conftest.py plugins contain directory-specific hook implementations.
 Session and test running activities will invoke all hooks defined in
 conftest.py files closer to the root of the filesystem."""
-
-import pytest
-
-
-def pytest_addoption(parser):
-    parser.addoption(
-        "--runslow", action="store_true", default=False, help="run slow tests"
-    )
-
-
-def pytest_collection_modifyitems(config, items):
-    if config.getoption("--runslow"):
-        # --runslow given in cli: do not skip slow tests
-        return
-    skip_slow = pytest.mark.skip(reason="need --runslow option to run")
-    for item in items:
-        if "slow" in item.keywords:
-            item.add_marker(skip_slow)
