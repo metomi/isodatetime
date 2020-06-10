84dbb92b884ee31f2f47c93550a6b0d6201a0e22
Oliver Sanders <oliver.sanders@metoffice.gov.uk>
2020-06-10 17:03:32 +0100

    test_time_point: batch by century

--- metomi/isodatetime/tests/test_01.py
+++ metomi/isodatetime/tests/test_01.py
@@ -510,7 +510,6 @@ def run_comparison_tests(data_class, test_cases):
 class TestDataModel(unittest.TestCase):
     """Test the functionality of data model manipulation."""
 
-    @pytest.mark.slow
     def test_days_in_year_range(self):
         """Test the summing-over-days-in-year-range shortcut code."""
         for start_year in range(-401, 2):
--- metomi/isodatetime/tests/test_time_point.py
+++ metomi/isodatetime/tests/test_time_point.py
@@ -43,91 +43,85 @@ test_duration_attributes = [
 
 
 @pytest.mark.slow
-@pytest.mark.parametrize('test_year', range(1801, 2403))
-def test_timepoint(test_year):
+@pytest.mark.parametrize('century', range(18, 24))
+def test_timepoint(century):
     """Test the time point data model (takes a while).
 
-    For a range of years (e.g. 1801 to 2403) it iterates through each
+    For a range of years (e.g. 1800 to 2400) it iterates through each
     year, then creates another range with the days in this year. Finally
     performs a series of tests, failing if any operation results in
     an error."""
+    for test_year in range(century * 100, (century + 1) * 100, 1):
+        _test_timepoint(test_year)
+
+
+def _test_timepoint(test_year):
     my_date = datetime.datetime(test_year, 1, 1)
     stop_date = datetime.datetime(test_year + 1, 1, 1)
     for date in daterange(my_date, stop_date):
-        _do_test_dates(date)
-
-
-def _do_test_dates(my_date):
-    """Performs a series of tests against a given date.
-
-    This method does some time consuming operations, which are not IO
-    bound, so this method is a good candidate to be run concurrently.
-
-    :param my_date: a date to be tested
-    :type my_date: datetime.datetime
-    """
-    ctrl_data = my_date.isocalendar()
-    test_date = TimePoint(
-        year=my_date.year,
-        month_of_year=my_date.month,
-        day_of_month=my_date.day
-    )
-    test_week_date = test_date.to_week_date()
-    test_data = test_week_date.get_week_date()
-    assert test_data == ctrl_data
-    ctrl_data = (my_date.year, my_date.month, my_date.day)
-    test_data = test_week_date.get_calendar_date()
-    assert test_data == ctrl_data
-    ctrl_data = my_date.toordinal()
-    year, day_of_year = test_date.get_ordinal_date()
-    test_data = day_of_year
-    test_data += get_days_since_1_ad(year - 1)
-    assert test_data == ctrl_data
-    for attribute, attr_max in test_duration_attributes:
-        kwargs = {attribute: random.randrange(0, attr_max)}
-        ctrl_data = my_date + datetime.timedelta(**kwargs)
-        ctrl_data = ctrl_data.year, ctrl_data.month, ctrl_data.day
-        test_data = (
-            (test_date + Duration(**kwargs)).get_calendar_date())
+        ctrl_data = date.isocalendar()
+        test_date = TimePoint(
+            year=date.year,
+            month_of_year=date.month,
+            day_of_month=date.day
+        )
+        test_week_date = test_date.to_week_date()
+        test_data = test_week_date.get_week_date()
         assert test_data == ctrl_data
-        ctrl_data = my_date - datetime.timedelta(**kwargs)
-        ctrl_data = ctrl_data.year, ctrl_data.month, ctrl_data.day
-        # TBD: the subtraction is quite slow. Much slower than other
-        # operations. Could be related to the fact it converts the value
-        # in kwargs to negative multiplying by -1 (i.e. from __sub__ to
-        # __mul__), and also adds it to the date (i.e. __add__).
-        # Profiling the tests, the __sub__ operation used in the next
-        # line will appear amongst the top of time consuming operations.
-        test_data = (
-            (test_date - Duration(**kwargs)).get_calendar_date())
+        ctrl_data = (date.year, date.month, date.day)
+        test_data = test_week_date.get_calendar_date()
         assert test_data == ctrl_data
-    kwargs = {}
-    for attribute, attr_max in test_duration_attributes:
-        kwargs[attribute] = random.randrange(0, attr_max)
-    test_date_minus = test_date - Duration(**kwargs)
-    test_data = test_date - test_date_minus
-    ctrl_data = Duration(**kwargs)
-    assert test_data == ctrl_data
-    test_data = test_date_minus + (test_date - test_date_minus)
-    ctrl_data = test_date
-    assert test_data == ctrl_data
-    test_data = (test_date_minus + Duration(**kwargs))
-    ctrl_data = test_date
-    assert test_data == ctrl_data
-    ctrl_data = (
-        my_date +
-        datetime.timedelta(minutes=450) +
-        datetime.timedelta(hours=5) -
-        datetime.timedelta(seconds=500, weeks=5))
-    ctrl_data = [
-        (ctrl_data.year, ctrl_data.month, ctrl_data.day),
-        (ctrl_data.hour, ctrl_data.minute, ctrl_data.second)]
-    test_data = (
-        test_date + Duration(minutes=450) +
-        Duration(hours=5) -
-        Duration(weeks=5, seconds=500)
-    )
-    test_data = [
-        test_data.get_calendar_date(),
-        test_data.get_hour_minute_second()]
-    assert test_data == ctrl_data
+        ctrl_data = date.toordinal()
+        year, day_of_year = test_date.get_ordinal_date()
+        test_data = day_of_year
+        test_data += get_days_since_1_ad(year - 1)
+        assert test_data == ctrl_data
+        for attribute, attr_max in test_duration_attributes:
+            kwargs = {attribute: random.randrange(0, attr_max)}
+            ctrl_data = date + datetime.timedelta(**kwargs)
+            ctrl_data = ctrl_data.year, ctrl_data.month, ctrl_data.day
+            test_data = (
+                (test_date + Duration(**kwargs)).get_calendar_date())
+            assert test_data == ctrl_data
+            ctrl_data = date - datetime.timedelta(**kwargs)
+            ctrl_data = ctrl_data.year, ctrl_data.month, ctrl_data.day
+            # TBD: the subtraction is quite slow. Much slower than other
+            # operations. Could be related to the fact it converts the
+            # value in kwargs to negative multiplying by -1 (i.e. from
+            # __sub__ to __mul__), and also adds it to the date (i.e.
+            # __add__).  Profiling the tests, the __sub__ operation used in
+            # the next line will appear amongst the top of time consuming
+            # operations.
+            test_data = (
+                (test_date - Duration(**kwargs)).get_calendar_date())
+            assert test_data == ctrl_data
+        kwargs = {}
+        for attribute, attr_max in test_duration_attributes:
+            kwargs[attribute] = random.randrange(0, attr_max)
+        test_date_minus = test_date - Duration(**kwargs)
+        test_data = test_date - test_date_minus
+        ctrl_data = Duration(**kwargs)
+        assert test_data == ctrl_data
+        test_data = test_date_minus + (test_date - test_date_minus)
+        ctrl_data = test_date
+        assert test_data == ctrl_data
+        test_data = (test_date_minus + Duration(**kwargs))
+        ctrl_data = test_date
+        assert test_data == ctrl_data
+        ctrl_data = (
+            date +
+            datetime.timedelta(minutes=450) +
+            datetime.timedelta(hours=5) -
+            datetime.timedelta(seconds=500, weeks=5))
+        ctrl_data = [
+            (ctrl_data.year, ctrl_data.month, ctrl_data.day),
+            (ctrl_data.hour, ctrl_data.minute, ctrl_data.second)]
+        test_data = (
+            test_date + Duration(minutes=450) +
+            Duration(hours=5) -
+            Duration(weeks=5, seconds=500)
+        )
+        test_data = [
+            test_data.get_calendar_date(),
+            test_data.get_hour_minute_second()]
+        assert test_data == ctrl_data
