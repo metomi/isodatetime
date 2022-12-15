45116cd097590940d284d2af8e241c2d08e257c6
Ronnie Dutta <61982285+MetRonnie@users.noreply.github.com>
2022-12-15 11:23:16 +0000

    Add support for Python 3.11

--- setup.py
+++ setup.py
@@ -103,6 +103,8 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3 :: Only",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Utilities"
